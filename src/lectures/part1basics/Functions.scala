70b4f83805563e809c7905a5427ece25b5da8a16
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-30 07:31:53 +0200

    Practicing recursion

---
+++ src/lectures/part1basics/Functions.scala
@@ -0,0 +1,42 @@
+package lectures.part1basics
+
+object Functions extends App{
+  def aFunction(a: String, b: Int): String = {
+    a + " " + b
+  }
+
+  // EXERCISES
+
+  // Greeting function.
+  def aGreeting(name: String, age: Int): String = {
+    s"Hi, my name is $name and I am $age years old!"
+  }
+
+//  println(aGreeting("John", 30))
+
+  // Factorial function.
+
+  def factorial(n: Int): Long = {
+    if (n <= 1) 1
+    else
+    n * factorial(n - 1) // n times the return value of the function.
+  }
+
+  def fibonacci(n: Int): Long = {
+    if (n <= 1) n
+    else fibonacci(n - 1) + fibonacci(n - 2)
+  }
+
+  def isPrime(n: Int): Boolean = {
+    def isPrimeUntil(t: Int): Boolean  = {
+      if (t <= 1) true
+      else (n % t != 0) && isPrimeUntil(t - 1)
+    }
+    if (n <= 1) false
+    else isPrimeUntil(n / 2)
+  }
+
+//  println(factorial(5))
+//  println(fibonacci(3))
+  println(isPrime(0))
+}
--- src/playground/ScalaPlayground.scala
+++ src/playground/ScalaPlayground.scala
@@ -2,4 +2,30 @@ package playground
 
 object ScalaPlayground extends App{
   println("Hello, Scala!")
+
+  def factorial(n: Int): Int = {
+    if (n <= 1) 1
+    else n * factorial(n - 1)
+  }
+
+  def fibonacci(n: Int): Int = {
+    if (n <= 1) n
+    else fibonacci(n - 1) + fibonacci(n - 2)
+  }
+
+  def isPrime(n: Int): Boolean  = {
+    def isPrimeUntil(t: Int): Boolean = {
+      if (t <= 1) true
+      else n % t != 0 && isPrimeUntil(t - 1)
+    }
+    if (n <= 1) false
+    else isPrimeUntil(n / 2)
+  }
+
+  def gcd(a: Int, b: Int): Int = {
+    if (b == 0) a
+    else gcd(b, a % b)
+  }
+
+  println(gcd(117, 57))
 }
