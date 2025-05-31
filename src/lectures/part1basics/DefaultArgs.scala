2f9180c22d0236bcf22289f23ccea9c3ceaec2af
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-31 18:33:26 +0200

    Part 6, Default arguments, completed

--- src/lectures/part1basics/DefaultArgs.scala
+++ src/lectures/part1basics/DefaultArgs.scala
@@ -1,5 +1,20 @@
 package lectures.part1basics
 
 object DefaultArgs extends App {
+  def trFactorial(n: Int, acc: Int = 1): Int = { // Default argument for acc
+    if (n <= 1) acc
+    else trFactorial(n - 1, n * acc)
+  }
 
+  val factorial10 = trFactorial(10)
+
+  def savePicture(format: String = "jpg", width: Int = 1920, height: Int = 1080): Unit = println("saving picture")
+  savePicture(width = 800)
+
+  /*
+    1. pass in every leading argument
+    2. name the arguments
+   */
+
+  savePicture(height = 600, width = 800, format = "bmp")
 }
