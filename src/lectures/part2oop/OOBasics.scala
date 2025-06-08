42178a1c830eede93ba5d33100234c088f5da684
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-06-08 16:06:11 +0200

    Exercises for OOPBasics, completed

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -1,5 +1,7 @@
 package lectures.part2oop
 
+import java.time.LocalDate
+
 object OOBasics extends App {
 
   // Constructor
@@ -22,7 +24,71 @@ object OOBasics extends App {
     def this() = this("John Doe") // This will override the default argument 'Some name'
   }
 
+  class Author(name: String, surname: String, dateOfBirth: LocalDate) {
+
+    def fullName = s"$name $surname"
+
+    def age(): Int = {
+      val today = LocalDate.now()
+      today.getYear - dateOfBirth.getYear - {
+        if (today.getDayOfYear < dateOfBirth.getDayOfYear) 1
+        else 0
+      }
+    }
+  }
+
+  class Novel(name: String, dateOfRelease: LocalDate, author: Author) {
+
+    def authorAge() = s"The author of '$name' is ${author.age} years old."
+
+    def isWrittenBy() = s"'$name' is written by ${author.fullName}."
+
+    def wasReleased() = s"This copy of '$name' was released in ${dateOfRelease.getYear}."
+
+    def copy(newYearOfRelease: LocalDate): Novel =
+      new Novel(name, newYearOfRelease, author)
+  }
+
+  class Counter(val value: Int = 0) {
+    def currentCount() = s"This counter has a value of $value."
+
+    def increment(): Counter = {
+      println("Incrementing")
+      new Counter(value + 1)
+    }
+    def increment(n: Int): Counter = {
+      if (n <= 0) {
+        println(s"This counter has a value of ${value}.")
+        this
+      }
+      else increment().increment(n - 1)
+    }
+
+    def decrement(): Counter = {
+      println(value)
+      new Counter(value - 1)
+    }
+    def decrement(n: Int): Counter = {
+      if (n <= 0) this
+      else decrement().decrement(n - 1)
+    }
+
+  }
+
   val person = new Person()
   person.greet()
   person.greet("Bob")
+
+  val someAuthor = new Author("Bob", "Boberson", LocalDate.of(1990, 10, 15))
+  val someNovel = new Novel("The best Novel", LocalDate.of(2021, 10, 10), someAuthor)
+  val novelCopy = someNovel.copy(LocalDate.now())
+
+  println(someNovel.authorAge())
+  println(someNovel.isWrittenBy())
+  println(someNovel.wasReleased())
+  println(novelCopy.wasReleased())
+
+  val counter = new Counter()
+  println(counter.currentCount())
+  counter.increment(10)
 }
