ad95756f062aaebc666a710954f7df92550e4592
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-06-30 16:02:22 +0200

    Map Flatmap and filter

---
+++ src/lectures/part3fp/MapFlatmapFilter.scala
@@ -0,0 +1,29 @@
+package lectures.part3fp
+
+object MapFlatmapFilter extends App {
+
+  val list = List(1, 2, 3)
+  println(list.head)
+  println(list.tail)
+
+  // map
+  println(list.map(_ + 1))
+//  println(list.map(_ + " is a number"))
+
+  // filter
+  println(list.filter(_ % 2 == 0))
+
+  // flatMap
+  val toPair = (x: Int) => List(x, x + 1)
+  println(list.flatMap(toPair))
+
+  val numbers = List(1,2,3,4)
+  val chars = List('a','b','c','d')
+  val colors = List("black", "white")
+
+  // List("a1", "a2"... "d4")
+
+  // "iterating"
+  val combinations = numbers.filter(_ == 1).flatMap(n => chars.flatMap(c => colors.map(color => s"$n$c-$color")))
+  println(combinations)
+}
--- src/playground/ScalaPlayground.scala
+++ src/playground/ScalaPlayground.scala
@@ -1,5 +1,13 @@
 package playground
 
 object ScalaPlayground extends App {
-  println("Hello, Scala!")
+  val forComprehension = for {
+    x <- List(1, 2, 3)
+    y <- List(10, 20)
+    if x > 1
+  } yield x * y
+
+  println(forComprehension)
+
+  println(List(1).init.length)
 }
