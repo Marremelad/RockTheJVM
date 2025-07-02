3a08c951d5cf9fbce9cb9fb9043523fe68040610
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-07-02 14:28:23 +0200

    Scala99

--- src/playground/ScalaPlayground.scala
+++ src/playground/ScalaPlayground.scala
@@ -1,5 +1,10 @@
 package playground
 
 object ScalaPlayground extends App {
-  println("Hello Scala!")
+
+  val listOne = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val listTwo = List('a', 'b', 'c')
+  val testList = List(2, 3 ,1)
+
+  println(listOne.reverse.tail.head)
 }
